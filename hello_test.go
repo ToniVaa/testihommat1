a65aec1789ed6b5cc7555ccb1419f9be887ca3f9
Toni Vaahtera <tonivaa@pm.me>
2022-07-14 20:38:49 +0300

    empty string defaults to "word"

--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,23 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assetCorrectMessage := func(t testing.TB, got,want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+	
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+		assetCorrectMessage(t, got, want)
+	})
+
+	t.Run("empty string defaults to 'world", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, world"
+		assetCorrectMessage(t, got, want)
+	})
 }
