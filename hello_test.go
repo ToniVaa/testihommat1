f79f8364e182942ac50fc41bc4e29078ae4dd988
Toni Vaahtera <tonivaa@pm.me>
2022-07-14 20:43:27 +0300

    fixing a typo

--- hello_test.go
+++ hello_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestHello(t *testing.T) {
 
-	assetCorrectMessage := func(t testing.TB, got,want string) {
+	assertCorrectMessage := func(t testing.TB, got,want string) {
 		t.Helper()
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
@@ -14,12 +14,12 @@ func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
 		got := Hello("Chris")
 		want := "Hello, Chris"
-		assetCorrectMessage(t, got, want)
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("empty string defaults to 'world", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, world"
-		assetCorrectMessage(t, got, want)
+		assertCorrectMessage(t, got, want)
 	})
 }
